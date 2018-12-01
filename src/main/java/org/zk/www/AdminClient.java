2bef9c8c6d5cbe77bd1d87c047523f1b61420727
liufa <704759810@qq.com>
2018-12-01 22:24:47 +0800

    master

--- src/main/java/org/zk/www/AdminClient.java
+++ src/main/java/org/zk/www/AdminClient.java
@@ -1,8 +1,11 @@
 package org.zk.www;
 
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.KeeperException.NodeExistsException;
+import org.apache.zookeeper.data.Stat;
 
 public class AdminClient implements Watcher{
 	ZooKeeper zk;
@@ -10,6 +13,20 @@ public class AdminClient implements Watcher{
 	public AdminClient(String hostPort) {
 		this.hostPort = hostPort;
 	}
+	
+	void listState() throws KeeperException {
+		Stat stat = new Stat();
+		try {
+			byte[] masterDate =zk.getData("/master", false, stat);
+		} catch (NodeExistsException e) {
+			System.out.println("No Master");
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+	}
+	
 	public void process(WatchedEvent arg0) {
 		System.out.println("argo");
 	}
